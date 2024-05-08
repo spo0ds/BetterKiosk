7fafdc7dfac811398d405e146cacfe6795f93881
spo0ds <addresszero789@proton.me>
2024-05-08 15:28:53 +0545

    feat: policy added for nft

--- scripts/utils/packageInfo.ts
+++ scripts/utils/packageInfo.ts
@@ -1,7 +1,7 @@
 export let packageId = '0xc8d8ad63d99246e887ae5ed7c1999a4f932954108403a755038340fd304633f8';
 export let nftCap = '0x395449b1e20bb73dbf950cc38d3cdf65adcd2ba9ca3accdc5b68dc2ccbdf8f5d';
-export let TransferPolicyCapId = '0xf8df2f682f709ef4a5527a4e9bbbe8b6bb454d187c5d3788bdc0fa71491edab9';
-export let TransferPolicyId = '0x30abd03127fb1082a1a2ce606095b3afe757086fa13ed295ec0acce6d86f0b18';
+export let TransferPolicyCapId = '0x30abd03127fb1082a1a2ce606095b3afe757086fa13ed295ec0acce6d86f0b18';
+export let TransferPolicyId = '0xf8df2f682f709ef4a5527a4e9bbbe8b6bb454d187c5d3788bdc0fa71491edab9';
 
 export let Kiosk = '0xf85c7887594a73d7f45b8e0d8a8c2bfd81b7b819b93510a8c725e33138fcfd8e';
 export let KioskOwnerCap = '0x6dd805cf089762267711d25401391db2020de9fd286de74938a19086517f9340';
