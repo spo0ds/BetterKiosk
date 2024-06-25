799ca6bffc8be94a26f91f6cc78e3a91afea1afc
spo0ds <addresszero789@proton.me>
2024-06-25 12:53:46 +0545

    feat: script to request for nft approve to list in kiosk

---
+++ scripts/src/requestApprove.ts
@@ -0,0 +1,31 @@
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+import * as dotenv from 'dotenv';
+import getExecStuff from '../utils/execStuff';
+import { packageId, Kiosk, nftId } from '../utils/packageInfo';
+dotenv.config();
+
+
+async function requestApprove() {
+    const { keypair, client } = getExecStuff();
+    const tx = new TransactionBlock();
+    const pt = tx.moveCall({
+        target: `${packageId}::kiosk::request_approve_for_list`,
+        arguments: [
+            tx.object(Kiosk),
+            tx.object(nftId),
+            tx.pure.u64(10000000),
+        ],
+        typeArguments: [`${packageId}::nft::NFT`]
+
+    });
+
+    const result = await client.signAndExecuteTransactionBlock({
+        signer: keypair,
+        transactionBlock: tx,
+    });
+    console.log({ result });
+    console.log(pt);
+}
+
+
+requestApprove();
